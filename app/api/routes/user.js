d02e3371e30b76a0167ae60cb8040d7d5b5e4377
Ken Miller <5077371+ckap@users.noreply.github.com>
2021-10-05 15:21:55 -0400

    Updated user route

--- app/api/routes/user.js
+++ app/api/routes/user.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/test', (req, res) => {
-  res.json({ yee: 'haw' })
+router.get('/profile', (req, res) => {
+  res.json({
+    id: 1,
+    name: 'Michael Scott',
+    birthday: Date('10/5/1980'),
+    joined: new Date()
+  })
 })
 
 module.exports = router
