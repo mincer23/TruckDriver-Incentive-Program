61bfcd8687da789d0bba5b8eb4389b4e4f232f61
Mehrwan Namiranian <mehrwan@clemson.edu>
2021-10-05 19:28:01 +0000

    Added loginPage.vue to /

---
+++ loginPage.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="vue-template">
+        <form>
+            <h3> Sign Up </h3>
+            
+            <div class="form-group">
+                <label> Full Name </label>
+                <input type = "text" class ="form-control form-control-lg"/>
+            </div>
+
+            <div class="form-group">
+                <label> E-Mail Address </label>
+                <input type = "text" class ="form-control form-control-lg"/>
+            </div>
+
+            <div class="form-group">
+                <label> Username </label>
+                <input type = "text" class ="form-control form-control-lg"/>
+            </div>
+
+            <div class="form-group">
+                <label> Password </label>
+                <input type = "text" class ="form-control form-control-lg"/>
+            </div>
+
+
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return{}
+        }
+    }
+</script>
