d243cfd5a25f6fd228322f05deaa3a6c48131dbe
tylerhurst <wthurst@clemson.edu>
2021-10-07 15:52:05 -0400

    space/tab fixes

--- app/pages/login.vue
+++ app/pages/login.vue
@@ -1,23 +1,25 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 <template>
-    <div class="login">
-        <form>
-            <h1>Log In</h1>
-            <p></p>
-            <div class="form-input">
-                <label>Email Address</label>
-                <input type="email" class="form-control form-control-lg" />
-            </div>
-            <div class="form-input">
-                <label>Password</label>
-                <input type="password" class="form-control form-control-lg" />
-            </div>
-            <p></p>
-            <button type="submit" class="btn btn-primary btn-lg btn-square">Sign In</button>
-            <p class="forgot-password text-right mt-2 mb-4">
-                <router-link to="/forgot-password">Forgot password?</router-link>
-            </p>
-        </form>
-    </div>
+  <div class="login">
+    <form>
+      <h1>Log In</h1>
+      <p />
+      <div class="form-input">
+        <label>Email Address</label>
+        <input type="email" class="form-control form-control-lg">
+      </div>
+      <div class="form-input">
+        <label>Password</label>
+        <input type="password" class="form-control form-control-lg">
+      </div>
+      <p />
+      <button type="submit" class="btn btn-primary btn-lg btn-square">Sign In</button>
+      <p class="forgot-password text-right mt-2 mb-4">
+        <router-link to="/forgot-password">Forgot password?</router-link>
+      </p>
+    </form>
+  </div>
 </template>
 
 <script>
