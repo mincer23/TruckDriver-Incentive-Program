9e3ed07804bb120a74f90117d229ce388385c4b3
Tyler Hurst <wthurst@clemson.edu>
2021-10-19 18:09:18 +0000

    Added information change and password change pages

---
+++ info-change.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="info-change">
+    <form>
+      <h1>Edit Account Information</h1>
+      <p></p>
+      <div class="form-username">
+        <label>Username</label>
+        <input type="username" class="form-control" id="changeUsername" placeholder="Enter username..." />
+      </div>
+      <p></p>
+      <div class="form-email">
+        <label>Email</label>
+        <input type="email" class="form-control" id="changeEmail" placeholder="Enter email..." />
+      </div>
+      <p></p>
+      <div class="form-phone"> 
+        <label>Phone Number</label>
+        <input type="text" class="form-control" id="changePhone" placeholder="Enter phone number..." />
+      </div>
+      <p></p>
+      <button type="submit" class="btn btn-primary btn-lg btn-square">Submit</button>
+    </form>
+  </div>
+</template>
+
+<script>
+
+</script>
---
+++ password-change.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="password-change">
+    <form>
+      <h1>Change Password</h1>
+      <p></p>
+      <div class="form-password">
+        <label>New Password</label>
+        <input type="password" class="form-control" id="newPassword" placeholder="Enter new password..." />
+      </div>
+      <p></p>
+      <div class="form-confirm-password">
+        <label>Confirm New Password</label>
+        <input type="password" class="form-control" id="confirmPassword" placeholder="Confirm..." />
+      </div>
+      <p></p>
+      <button type="submit" class="btn btn-primary btn-lg btn-square">Submit</button>
+    </form>
+  </div>
+</template>
+
+<script>
+
+</script>
