9958afbe92301ba3688c01562771be4c0e41cfb7
Mehrwan Namiranian <mehrwan@clemson.edu>
2021-10-19 19:22:14 +0000

    Added 8 files. Includes Sign up and profile pages.

---
+++ index.vue
@@ -0,0 +1,14 @@
+<template>
+    
+  <section class="section">
+    <div class="container">
+      <h1 class="title">Home</h1>
+      <h1 class="title">Use Nav Bar!</h1>
+    </div>
+  </section>
+
+
+</template>
+
+
+
