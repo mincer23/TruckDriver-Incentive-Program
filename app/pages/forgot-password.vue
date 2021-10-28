2865f16486523e355d42c66a26a3631c418a43f8
Tyler Hurst <wthurst@clemson.edu>
2021-10-28 11:33:43 -0400

    update to forgot-password

--- app/pages/forgot-password.vue
+++ app/pages/forgot-password.vue
@@ -5,7 +5,7 @@
       <p></p>
       <div class="email">
         <label>Please enter the email for your account</label>
-        <input type="email" class="form-control" id="email" placeholder="you@example.com..." required />
+        <input v-model="email" type="email" class="form-control" placeholder="you@example.com..." required>
       </div>
       <p></p>
       <button type="submit" class="btn btn-primary btn-lg btn-square">Submit</button>
@@ -14,5 +14,24 @@
 </template>
 
 <script>
-
+/* import { mapMutations } from 'vuex'
+export default {
+  data () {
+    return {
+      email: null
+    }
+  },
+  methods: {
+    ...
+    async onSumbit(event) {
+      event.preventDefault()
+      const data = {
+        email: this.email
+      }
+      const result = await this.$http.$post('/api/', data)
+  } */
 </script>
+
+<style scoped>
+
+</style>
