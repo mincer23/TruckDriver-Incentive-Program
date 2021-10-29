185b4a79480a13dabf4f9f4553a760d925747e4d
Tyler Hurst <wthurst@clemson.edu>
2021-10-29 12:58:51 -0400

    Updated login page

--- app/pages/login.vue
+++ app/pages/login.vue
@@ -1,29 +1,35 @@
 <template>
-  <div class="login m-5">
-    <form @submit="onSubmit">
-      <h1>Log In</h1>
-      <br>
-      <div class="form-input">
-        <label>Username</label>
-        <input v-model="userName" name="username" type="text" class="form-control form-control-lg">
+  <div class="card text-center w-75">
+    <div class="card-body">
+      <div class="login m-5">
+        <form @submit="onSubmit">
+          <h1 class="card-title">Log In</h1>
+          <br>
+          <div class="form-input">
+            <label>Username</label>
+            <input v-model="userName" name="username" type="text" class="form-control form-control-lg">
+          </div>
+          <br>
+          <div class="form-input">
+            <label>Password</label>
+            <input v-model="password" name="password" type="password" class="form-control form-control-lg">
+          </div>
+          <br>
+          <button type="submit" class="btn btn-primary btn-lg btn-square">Sign In</button>
+          <div class="forgot-password text-right">
+            <NuxtLink to="/forgot-password">
+              Forgot password?
+            </NuxtLink>
+          </div>
+          <br>
+          <div class="create-account text-right">
+            <NuxtLink to="/singupPage">
+              New? Create an account!
+            </NuxtLink>
+          </div>
+        </form>
       </div>
-      <div class="form-input">
-        <label>Password</label>
-        <input v-model="password" name="password" type="password" class="form-control form-control-lg">
-      </div>
-      <br>
-      <button type="submit" class="btn btn-primary btn-lg btn-square">Sign In</button>
-      <p class="create-account text-right mt-2 mb-4">
-        <NuxtLink to="/singupPage">
-          New? Create an account!
-        </NuxtLink>
-      </p>
-      <p class="forgot-password text-right mt-2 mb-4">
-        <NuxtLink to="/forgot-password">
-          Forgot password?
-        </NuxtLink>
-      </p>
-    </form>
+    </div>
   </div>
 </template>
 
@@ -60,5 +66,7 @@ export default {
 </script>
 
 <style scoped>
-
+.card {
+  margin: auto;
+}
 </style>
