eeab0ef47834bb2960930a62f97dc23ae393147c
Ken Miller <5077371+ckap@users.noreply.github.com>
2021-10-29 10:35:51 -0400

    Updated index.vue to contain a generic splash screen on login
    Don't worry Mehrwan, I'll put the truck back for people who aren't signed in!

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,12 +1,48 @@
 <template>
-    <div class="container">
-      <h1 class="title">Home</h1>
-      <img src="/truck.jpeg" >
+  <div>
+    <div v-if="getUser">
+      <SplashContainer
+        :first-name="getUser.firstName"
+        :last-name="getUser.lastName"
+        :transactions="test_transactions"
+        :balance="Number(40000)"
+      />
+    </div>
+    <div v-else>
+      <span>Please <b-button to="/login" variant="primary">Login</b-button> to view this page.</span>
+    </div>
   </div>
 </template>
+
 <script>
+import { mapGetters } from 'vuex'
 export default {
-  setup () {
+  data () {
+    return {
+      test_transactions: [
+        {
+          sponsor: 'The Meta',
+          totalPoints: 42069,
+          pointChange: 69,
+          id: 3
+        },
+        {
+          sponsor: 'The Meta',
+          totalPoints: 42000,
+          pointChange: 2000,
+          id: 2
+        },
+        {
+          sponsor: 'The Meta',
+          totalPoints: 40000,
+          pointChange: -420,
+          id: 1
+        }
+      ]
+    }
+  },
+  computed: {
+    ...mapGetters(['getUser'])
   }
 }
 </script>
