ee8b647029d859eb23ea77c8b05f69e82cceb2e4
Ken Miller <5077371+ckap@users.noreply.github.com>
2021-11-11 17:01:30 -0500

    and more

--- app/api/utilities.js
+++ app/api/utilities.js
@@ -7,7 +7,7 @@ function ensureAdmin (req, res, next) {
   res.redirect('/')
 }
 
-// Simple route middleware to ensure user is a sponsor.
+// Simple route middleware to ensure user is a sponsor or an admin.
 function ensureSponsor (req, res, next) {
   if (req?.session?.user?.isAdmin || req?.session?.user?.sponsorFor?.length > 0) { return next() }
   res.redirect('/')
@@ -19,8 +19,15 @@ function ensureAuthenticated (req, res, next) {
   res.redirect('/')
 }
 
+// zxcvbn password handling
+function isPasswordStrong (req, res, next) {
+  // this will do shit later trust me -- ken
+  return true
+}
+
 export {
   ensureAdmin,
   ensureSponsor,
-  ensureAuthenticated
+  ensureAuthenticated,
+  isPasswordStrong
 }
