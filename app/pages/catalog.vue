dde3c7d9953ebd4cdb1ffaa90b8a44d015aaaaea
mincer23 <mehrwan69@gmail.com>
2021-11-14 16:28:44 -0500

    Added search bar and dropdowns for sorting

--- app/pages/catalog.vue
+++ app/pages/catalog.vue
@@ -2,6 +2,19 @@
     <section class = "section">
      <h1 class="card-title">Catalog</h1>
     <b-container v-if="data">
+      <div class = "search-bar">
+        <b-form-input
+          @input = "search_text()"
+          v-model="search.text"
+          type = "text"
+          placeholder="Search for Item"
+        ></b-form-input>
+
+        <b-dropdown text="Sort by">
+          <b-dropdown-item href="#">Ascending</b-dropdown-item>
+          <b-dropdown-item href="#">Descending</b-dropdown-item>
+        </b-dropdown>
+      </div>
         <b-row>
         <b-col v-for="item in data.results" :key="item.listing_id" cols="12">
             <b-card img-right>
@@ -38,15 +51,27 @@ export default {
     }
   },
   mehtods: {
-    addCart (item) {
+    async addCart (item) {
+      item.preventDefault
+      const data = {
+        item = this.item
+      }
       this.$emit('update-cart', item)
+    },
+  
+    async fetch () {
+      this.data = await fetch('https://openapi.etsy.com/v2/listings/active?api_key=vh0cf53nxhvc871sc5b2eabr').then(res => res.json())
+      for (let i = 0; i < this.data.results.length && i < 8; i++) {
+        this.data.results[i].imageSrc = await fetch('https://openapi.etsy.com/v2/listings/' + this.data.results[i].listing_id + '/images?api_key=vh0cf53nxhvc871sc5b2eabr').then(res => res.json()).then(result => result?.results[0]?.url_170x135)
+      }
+    },
+    sortAsc(data){
+      return _.orderBy(data, ' title', 'asc');
+    },
+    sortDesc(data){
+      return _.orderBy(data, ' title', 'desc');
     }
-  },
-  async fetch () {
-    this.data = await fetch('https://openapi.etsy.com/v2/listings/active?api_key=vh0cf53nxhvc871sc5b2eabr').then(res => res.json())
-    for (let i = 0; i < this.data.results.length && i < 8; i++) {
-      this.data.results[i].imageSrc = await fetch('https://openapi.etsy.com/v2/listings/' + this.data.results[i].listing_id + '/images?api_key=vh0cf53nxhvc871sc5b2eabr').then(res => res.json()).then(result => result?.results[0]?.url_170x135)
-    }
+
   }
 }
 </script>
