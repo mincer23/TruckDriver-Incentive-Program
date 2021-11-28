4a4852ee98f106cf8caf5a6a06fd78bbc730ea2f
Tyler Hurst <wthurst@clemson.edu>
2021-11-28 04:10:25 -0500

    LIKELY BROKEN CreateAdmin page and link to page

--- app/pages/admin/index.vue
+++ app/pages/admin/index.vue
@@ -17,6 +17,9 @@
                   <b-button variant="primary" to="/admin/manage/catalogs">Manage Catalogs</b-button>
                 </b-col>
                 <b-col cols="12" class="d-flex justify-content-center py-3">
+                  <b-button variant="primary" to="/admin/manage/createAdmin">Add Admin User</b-button>
+                </b-col>
+                <b-col cols="12" class="d-flex justify-content-center py-3">
                   <b-button variant="primary" to="/admin/manage/reports">View Reports</b-button>
                 </b-col>
               </b-row>
